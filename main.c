3ca34f4459dd45ac40113cce63a5b6e2b59adfd1
clara <clara.chalayer@edu.ece.fr>
2023-04-19 16:22:36 +0200

    J'ai rangé les fichiers dans le projet pour que ce soit plus simple

--- main.c
+++ main.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_primitives.h>
+#include "src/pecheAuxCanards/pecheAuxCanards.h"
+#include "/src/tirAuxBallons/tirAuxBallons.h"
+
 
 int main(void) {
     printf("Hello World!\n");
